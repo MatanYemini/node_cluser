3eb5b4bdc6d4a1fcd757affe7f201d4aa999b95c
Yemini <yeminimatan@gmail.com>
2020-07-02 19:34:18 +0300

    Adding server with clustring and without, also configure pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Express App',
+      script: 'server.js',
+      instances: 'MAX',
+      autorestart: true,
+      watch: true,
+      max_memory_restart: '1G',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: 'node',
+      host: '212.83.163.1',
+      ref: 'origin/master',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/production',
+      'post-deploy':
+        'npm install && pm2 reload ecosystem.config.js --env production',
+    },
+  },
+};
--- src/server.js
+++ src/server.js
@@ -1,30 +1,12 @@
 const express = require('express');
 const fabObj = require('./math-logic/fibonacci-series');
-const cluster = require('cluster');
-const totalCpus = require('os').cpus().length;
 
-if (cluster.isMaster) {
-  for (let i = 0; i < totalCpus; i++) {
-    cluster.fork();
-  }
-  cluster.on('online', (worker) => {
-    console.log(
-      `Worker Id is ${worker.id} and the PID is: ${worker.process.pid}`
-    );
-  });
-  cluster.on('exit', (worker) => {
-    console.log(
-      `Worker Id ${worker.id} with PID ${worker.process.pid} is offline`
-    );
-  });
-} else {
-  const app = express();
-  app.get('/', (req, res) => {
-    let number = fabObj.calculateFibonacciValue(
-      Number.parseInt(req.query.number)
-    );
-    res.send(`<h1>${number}</h1>`);
-  });
+const app = express();
+app.get('/', (req, res) => {
+  let number = fabObj.calculateFibonacciValue(
+    Number.parseInt(req.query.number)
+  );
+  res.send(`<h1>${number}</h1>`);
+});
 
-  app.listen(3000, () => console.log('Express server is running on port 3000'));
-}
+app.listen(3000, () => console.log('Express server is running on port 3000'));
