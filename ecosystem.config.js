b0ea5c3e6330bedfc3ca18f00f44e1a317206a72
Yemini <yeminimatan@gmail.com>
2020-07-06 14:18:23 +0300

    Changing config for workers and instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,17 +19,11 @@ module.exports = {
       name: 'FirstWorker',
       script: './src/workers/fib-worker1.js',
       instances: 1,
-      env: {
-        NODE_ENV='development',
-      }
     },
     {
       name: 'SecondWorker',
       script: './src/workers/fib-worker2.js',
       instances: 1,
-      env: {
-        NODE_ENV='development',
-      }
     },
   ],
 };
